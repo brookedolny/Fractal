e5431c17ba1f608c1cd08a615cf8afe5ed6050bb
Brooke Dolny <brookedolny@gmail.com>
2018-06-05 21:32:55 -0400

    Code cleanup

--- src/Main.java
+++ src/Main.java
@@ -17,14 +17,6 @@ public class Main {
         image.drawImage();
         System.out.println("time to draw image: " + (System.currentTimeMillis() - time));
         image.saveImage("./");
-//        for(double d = 2.0; d < 6; d += 0.01) {
-//            JuliaSetImage image = new JuliaSetImage( 0, -0.8, 1000, 1000, d, 4);
-//            time = System.currentTimeMillis();
-//            MandelbrotSetImage image = new MandelbrotSetImage(1000, 100, d, 1);
-//            image.drawImage();
-//            System.out.println("time to draw image: " + (System.currentTimeMillis() - time));
-//            image.saveImage("./");
-//        }
     }
 
 }
